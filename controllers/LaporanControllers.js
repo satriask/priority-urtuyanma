32fdd6c2f2be1ba9a0bc6b56cb5151bbc16bc847
ffznrr <satriask2026@gmail.com>
2026-08-08 11:19:41 +0700

    fix: bug pengirim

--- controllers/LaporanControllers.js
+++ controllers/LaporanControllers.js
@@ -1,18 +1,47 @@
-const { Op } = require("sequelize");
 const { Surat, PenilaianSurat } = require("../models");
 
 class LaporanControllers {
   static async getLaporan(req, res) {
     try {
-      const data = await Surat.findAll();
+      const data = await Surat.findAll({
+        include: [
+          {
+            model: PenilaianSurat,
+            as: "PenilaianSurats",
+            attributes: ["C4"],
+          },
+        ],
+      });
 
-      const hasil = data.map((item) => ({
-        id: item.id,
-        nomorSurat: item.nomorSurat,
-        judulSurat: item.judulSurat,
-        tanggalInput: item.tanggalSurat,
-        status: item.statusSurat,
-      }));
+      const getPengirim = (c4) => {
+        switch (c4) {
+          case 1:
+            return "Masyarakat / Perorangan";
+          case 2:
+            return "Satuan Kerja Internal";
+          case 3:
+            return "Instansi Pemerintah";
+          case 4:
+            return "Polda Metro Jaya";
+          case 5:
+            return "Mabes Polri";
+          default:
+            return "-";
+        }
+      };
+
+      const hasil = data.map((item) => {
+        const c4 = item.PenilaianSurats?.C4;
+
+        return {
+          id: item.id,
+          nomorSurat: item.nomorSurat,
+          judulSurat: item.judulSurat,
+          pengirimSurat: getPengirim(c4),
+          tanggalInput: item.tanggalSurat,
+          status: item.statusSurat,
+        };
+      });
 
       res.status(200).json({
         success: true,
@@ -22,6 +51,7 @@ class LaporanControllers {
       });
     } catch (error) {
       console.log(error);
+
       res.status(500).json({
         success: false,
         message: error.message,
